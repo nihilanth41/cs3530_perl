422961de119830562a084f95b58db688d23b4489
Zachary Rump <zrrm74@mail.missouri.edu>
2016-10-25 13:27:45 -0500

    Fix countfiles.pl

--- countfiles/countfiles.pl
+++ countfiles/countfiles.pl
@@ -8,12 +8,15 @@ my $ARGC = scalar @ARGV;
 $ARGC >= 1 or die "Error: not enough arguments.\nUsage is: $0 </path/to/dir>\n";
 
 my $file_counter = 0;
-my $dir = shift;
-find(\&count, $dir);
 
-print "Count: $file_counter\n";
+my $dir = shift;
+if (-d $dir)
+{
+	find(\&count, $dir);
+	print "Count: $file_counter\n";
+}
 
 sub count { 
-	$file_counter++;
+	$file_counter++ if -f;
 }
 
