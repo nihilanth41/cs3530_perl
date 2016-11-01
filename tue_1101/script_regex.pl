0e1e41bfde6d5caf21d00ea210679c2354164513
Zachary Rump <zrrm74@mail.missouri.edu>
2016-11-01 13:06:36 -0500

    Grab script tags

--- tue_1101/script_regex.pl
+++ tue_1101/script_regex.pl
@@ -4,6 +4,14 @@ use warnings;
 
 # http://perlmaven.com/here-documents
 
+# Demonstration of here str
+# END_HTML == end marker
+
+# determine if <script></script>A
+# extract it
+
+# script lang=""
+# ensure lang 
 my $string = <<'END_HTML';
 <html>
 <head>
@@ -25,4 +33,10 @@ This is my web page.
 </body>
 </html>
 END_HTML
+
+if($string =~ /<script>([\w|\W|\s|\r]*)<\/script>/)
+{
+	print "Found\n";
+	print $1 if defined $1;
+}
 
--- tue_1101/words_regex.pl
+++ tue_1101/words_regex.pl
@@ -4,3 +4,21 @@ use warnings;
 
 my $string = "The big fox and the little fox played in the woods.";
 
+# Replace fox with cat
+$string =~ s/fox/cat/g;
+print $string, "\n";
+
+# Replace the with 'a' but case sensitive
+# Chllnge: single regex to find {The or the} replace with correct case and only twice
+$string =~ s/^the/A/i;
+print "$string\n";
+
+# Squeeze spaces out
+# Match whitespace 1 or more times
+$string =~ s/\s+//g;
+print "$string\n";
+
+
+
+
+
