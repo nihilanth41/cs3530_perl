ffbbd59e8b8d35029688c891efad90462017ffb9
Zachary Rump <zrrm74@mail.missouri.edu>
2016-11-03 12:51:19 -0500

    Check parens

--- thu_1103/phone_regex.pl
+++ thu_1103/phone_regex.pl
@@ -18,7 +18,7 @@ print $string, "\n";
 
 # ###-###-####
 # Dashes could be space, no space, slashes
-if($string =~ /([2-9]\d{2})([\s|\-|\.|\/]?\d{3})[\s|\-|\.|\/]?(\d{4})/)
+if($string =~ /(\(?[2-9]\d{2}\)?)([\s|\-|\.|\/]?\d{3})[\s|\-|\.|\/]?(\d{4})/)
 {
 
 	#print "$1 $2 $3";
