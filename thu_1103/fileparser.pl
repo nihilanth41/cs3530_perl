49ac66b7b6b9b15529fdd72cb883b181d447b530
Zachary Rump <zrrm74@mail.missouri.edu>
2016-11-03 13:43:33 -0500

    70 matches

--- thu_1103/fileparser.pl
+++ thu_1103/fileparser.pl
@@ -13,3 +13,26 @@ while (my $line = <$fh>) {
 	$string .= $line;
 }
 
+#if($string =~ /<img[\s]+[.*]src="(.*)"\/>/) {
+
+# How to get array from regex KEY: GLOBAL
+#my @images = $string =~ /<regex>/g 
+
+# 50 matches:
+#my @images = $string =~ /<img[^src]*src="([^"]*)"[^>]*>/g;
+
+# 70 matches:
+# notation automatically assigns selection i.e. ()
+my @images = $string =~ /<img\s{1}[^>]*src="([^"]*)"[^>]*>/g;
+foreach(@images) 
+{
+	print $_, "\n\n";
+	
+}
+print $#images, "\n";
+#and urls with src=""
+# i.e. list of all images in the image tags
+#<img src=""/>
+#<img class="" src="">
+
+
