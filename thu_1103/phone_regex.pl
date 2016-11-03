203ca02057c3439cec02cddf8dd6e504ac15c563
Zachary Rump <zrrm74@mail.missouri.edu>
2016-11-03 13:02:16 -0500

    Ensure opening and closing parens

--- thu_1103/phone_regex.pl
+++ thu_1103/phone_regex.pl
@@ -12,13 +12,8 @@ my $string = shift @ARGV;
 print $string, "\n";
 
 
-# Another option (NOT 0 and NOT 1) 
-# Downside: matches non-digits
-#if($string =~ /([^0|^1]\d{2})-(\d{3})-(\d{4})/)
 
-# ###-###-####
-# Dashes could be space, no space, slashes
-if($string =~ /(\(?[2-9]\d{2}\)?)([\s|\-|\.|\/]?\d{3})[\s|\-|\.|\/]?(\d{4})/)
+if($string =~ /^(\([2-9]\d{2}\)|^[2-9]\d{2})([\s|\-|\.|\/]?\d{3})[\s|\-|\.|\/]?(\d{4})/)
 {
 
 	#print "$1 $2 $3";
