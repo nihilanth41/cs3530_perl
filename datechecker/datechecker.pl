bdbc87a6d0c5262911c4712c598436fb5806dce0
Zachary Rump <zrrm74@mail.missouri.edu>
2016-11-08 13:34:35 -0600

    Print output

--- datechecker/datechecker.pl
+++ datechecker/datechecker.pl
@@ -7,22 +7,27 @@ use warnings;
 ($#ARGV == 0) or die("Usage is: $0 <date string>\n");
 
 my $date_str = shift;
-if($date_str =~ /(\d{4}-\d{2}-\d{2}$)/)
+if($date_str =~ /(\d{4}-\d{2}-(\d{2})$)/)
 {
 	# Valid
-	print $1;
+	print $1, " is valid\n";
+	print "Year: ", $2, "\n";
 }
-elsif($date_str =~ /(\d{2}\/\d{2}\/\d{4}$)/)
+elsif($date_str =~ /(\d{2}\/\d{2}\/(\d{4})$)/)
 {
-	print $1;
+	print $1, " is valid\n";
+	print "Year: ", $2, "\n";
 }
-elsif($date_str =~ /(\d{2}\/\d{2}\/\d{2}$)/)
+elsif($date_str =~ /(\d{2}\/\d{2}\/(\d{2})$)/)
 {
-	print $1;
+	print $1, " is valid\n";
+	print "Year: ", $2, "\n";
 }
-elsif($date_str =~ /(^\s*[aA-zZ]{3}\s\d{2},\s*\d{4}$)/)
+#elsif($date_str =~ /(^\s*[aA-zZ]\s\d{2},\s*(\d{4})$)/)
+elsif($date_str =~ /(^\s*(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec){1}\s\d{2},\s*(\d{4})$)/)
 {
-	print $1;
+	print $1, " is valid\n";
+	print "Year: ", $3, "\n";
 }
 
 #YYYY-MM-DD
