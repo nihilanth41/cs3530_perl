8cad4675e436a2a98e29b45ee9232c4d494d9975
Zachary Rump <zrrm74@mail.missouri.edu>
2016-11-10 13:30:35 -0600

    Add comment

--- position1/position1.pl
+++ position1/position1.pl
@@ -5,13 +5,20 @@ use warnings;
 
 my $string = "toy='truck' color='red' cat='purrdy' dogs='beagle' car='toyota' doggy='mut' apple='delicious' dog='shepherd' colour='blue'";
 
+# The end position: $+[num] gives the location of the first character *after* the match
 while($string =~ /(dog[s|gy]*)='([^']*)'/g)
 {
-	print "$1 at ", $-[1],"-",$+[1]-1," type $2 at ", $-[2],"-",$+[2]-1, "\n";
+	if(defined $1 && defined $2)
+	{
+		print "$1 at ", $-[1],"-",$+[1]-1," type $2 at ", $-[2],"-",$+[2]-1, "\n";
+	}
 }
 
 # Same thing for colour
 while($string =~ /(colo[u]*r)='([^']*)'/g)
 {
-	print "$1 at ", $-[1],"-",$+[1]-1," type $2 at ", $-[2],"-",$+[2]-1, "\n";
+	if(defined $1 && defined $2)
+	{
+		print "$1 at ", $-[1],"-",$+[1]-1," type $2 at ", $-[2],"-",$+[2]-1, "\n";
+	}
 }
