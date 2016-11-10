2f2b46bb4904b2e171a484056ac851289e111b8e
Zachary Rump <zrrm74@mail.missouri.edu>
2016-11-10 13:18:06 -0600

    Match colour/color

--- position1/position1.pl
+++ position1/position1.pl
@@ -4,13 +4,14 @@ use strict;
 use warnings;
 
 my $string = "toy='truck' color='red' cat='purrdy' dogs='beagle' car='toyota' doggy='mut' apple='delicious' dog='shepherd' colour='blue'";
+
 while($string =~ /(dog[s|gy]*)='([^']*)'/g)
 {
 	print "$1 at ", $-[1],"-",$+[1]-1," type $2 at ", $-[2],"-",$+[2]-1, "\n";
 }
 
-# Locate dog, dogs, doggy
-# find =""
-# determine start and end of each instance e.g. dogs = i['d'] i['s']
-# determine start and end of each property value
-
+# Same thing for colour
+while($string =~ /(colo[u]*r)='([^']*)'/g)
+{
+	print "$1 at ", $-[1],"-",$+[1]-1," type $2 at ", $-[2],"-",$+[2]-1, "\n";
+}
