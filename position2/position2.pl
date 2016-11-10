ce052cfb373f8e3494d6f4ff1d667c0482513d35
Zachary Rump <zrrm74@mail.missouri.edu>
2016-11-10 13:28:48 -0600

    Check defined, add comment about character pos

--- position2/position2.pl
+++ position2/position2.pl
@@ -10,7 +10,11 @@ foreach my $property (@properties)
 {
 	while($string =~ /($property)='([^']*)'/g)
 	{
-		print "$1 at ", $-[1],"-",$+[1]-1," type $2 at ", $-[2],"-",$+[2]-1, "\n";
+		if(defined $1 && defined $2)
+		{
+			# The end position: $+[num] gives the location of the first character *after* the match
+			print "$1 at ", $-[1],"-",$+[1]-1," type $2 at ", $-[2],"-",$+[2]-1, "\n";
+		}
 	}
 }
 
